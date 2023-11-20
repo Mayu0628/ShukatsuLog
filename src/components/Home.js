813d0c9caf26d6b6abfe5ed3181ec6d442d622a1
Mayu0628 <c1101427@planet.kanazawa-it.ac.jp>
2023-11-20 15:55:49 +0900

    スタイル修正

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,44 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useEffect, useState } from "react";
 import { addDoc, collection, query, where, getDocs } from "firebase/firestore";
 import { auth, db } from "../firebase";
 import "./css/Home.css";
 
 export const Home = () => {
-  const [title, setTitle] = useState("");
-  const [urlInput, setUrlInput] = useState("");
-  const [urlData, setUrlData] = useState(null);
-  const [error, setError] = useState("");
-
-  const fetchData = useCallback(async () => {
-    if (!urlInput) return;
-
-    const data = {
-      key: "c5c75cc8e60e6bd554b556a03066c66e",
-      q: urlInput,
-    };
-
-    try {
-      const response = await fetch("https://api.linkpreview.net", {
-        method: "POST",
-        mode: "cors",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-      });
-      if (!response.ok) throw new Error("データ取得に失敗しました");
-
-      const json = await response.json();
-      setUrlData(json);
-    } catch (error) {
-      setError(error.message);
-    }
-  }, [urlInput]);
+  const [title, setTitle] = useState(""); // 入力されたタイトルを保持
+  const [urlInput, setUrlInput] = useState(""); // 入力されたURLを保持
+  const [urlData, setUrlData] = useState(null); // URLから取得したデータを保持
+  const [error, setError] = useState(""); // エラー用のステート
 
   useEffect(() => {
-    const timerId = setTimeout(() => fetchData(), 500);
-    return () => clearTimeout(timerId);
-  }, [urlInput, fetchData]);
+    const fetchData = async () => {
+      // urlInputが空の場合は何もしない
+      if (!urlInput) return;
+
+      const data = {
+        key: "c5c75cc8e60e6bd554b556a03066c66e",
+        q: urlInput,
+      };
+
+      try {
+        const response = await fetch("https://api.linkpreview.net", {
+          method: "POST",
+          mode: "cors",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data),
+        });
+        if (!response.ok) throw new Error("データ取得に失敗しました");
+
+        const json = await response.json();
+        setUrlData(json);
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+    fetchData();
+  }, [urlInput]);  // 依存配列にurlInputを指定
 
   const createPost = async () => {
+    // ログインしてない場合
     if (!auth.currentUser) {
       setError("ログインが必要です");
       return;
@@ -56,7 +56,7 @@ export const Home = () => {
       await addDoc(collection(db, "posts"), {
         title,
         url: {
-          image: urlData?.image || "../assets/No_image.jpeg",
+          image: urlData?.image ,
           title: urlData?.title,
           url: urlData?.url,
         },
@@ -69,7 +69,7 @@ export const Home = () => {
       setTitle("");
       setUrlInput("");
       setUrlData(null);
-      setError("投稿が追加されました");
+      window.alert("投稿が追加されました");
     } catch (error) {
       setError("投稿の追加に失敗しました: " + error.message);
     }
